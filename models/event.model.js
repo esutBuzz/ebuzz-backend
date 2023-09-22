c26a2b30fcab156ccf6c3b42b672d1bb4a59e137
kingsleycj <primekings.kc@gmail.com>
2023-09-22 22:45:00 +0100

    event model created

---
+++ models/event.model.js
@@ -0,0 +1,47 @@
+const mongoose = require('mongoose');
+
+const EventSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    required: true,
+  },
+  time: {
+    type: String,
+    required: true,
+  },
+  location: {
+    type: String,
+    required: true,
+  },
+  host: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  attendees: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+    },
+  ],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+  updatedAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const Event = mongoose.model('Event', EventSchema);
+
+module.exports = Event;
