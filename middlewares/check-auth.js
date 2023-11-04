1bb06e682b8450c1a7b1c336319226b70f45f67b
kingsleycj <primekings.kc@gmail.com>
2023-11-04 20:53:23 +0100

    minor omissions

--- middlewares/check-auth.js
+++ middlewares/check-auth.js
@@ -6,29 +6,4 @@ const isAuth = (req, res, next) => {
 }
 
 
-// const isAuth = (req, res, next) => {
-//     // Check if the user is logged in
-//     if (req.session.isLoggedIn) {
-//       // If the user is logged in, check the presence of a JWT token in the request headers
-//       const token = req.headers.authorization;
-  
-//       if (!token) {
-//         return res.status(401).json({ error: 'Unauthorized: No token provided' });
-//       }
-  
-//       // Verify the token
-//       jwt.verify(token, process.env.JWT_KEY, (err, decoded) => {
-//         if (err) {
-//           return res.status(401).json({ error: 'Unauthorized: Invalid token' });
-//         }
-  
-//         // If the token is valid, you can access the user's information from the 'decoded' object
-//         req.user = decoded;
-//         next();
-//       });
-//     } else {
-//       return res.status(403).json({ error: 'User must be logged in to perform this action' });
-//     }
-//   };
-
 module.exports = isAuth
